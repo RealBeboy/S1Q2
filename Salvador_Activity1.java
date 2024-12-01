a93045d3620ef30f753d0e1e136c6db51a227b7b
RealBeboy <93521745+RealBeboy@users.noreply.github.com>
2024-12-01 18:23:27 +0800

    Add files via upload

---
+++ Salvador_Activity1.java
@@ -0,0 +1,16 @@
+package semister2;
+import java.util.Scanner;
+public class Salvador_Activity1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Your Name?: ");
+		
+		String name = scanner.nextLine();
+		
+		System.out.println("Hello! " + name);
+		
+	}
+
+}
